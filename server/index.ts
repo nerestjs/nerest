5af131e12fad0b20785daf7310f7040e1a71e59b
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2022-10-03 10:22:02 +0300

    feat: add vite and fastify dependencies

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,20 @@
 // This is the nerest server entrypoint
+import fastify from 'fastify';
+import vite from 'vite';
 
-console.log('Hello Nerest Server');
+async function createServer() {
+  const root = process.cwd();
+
+  const viteSsr = vite.createServer({ root, appType: 'custom' });
+
+  const app = fastify();
+
+  app.get('/ping', () => 'pong');
+
+  return { app };
+}
+
+createServer().then(async ({ app }) => {
+  await app.listen({ port: 3000 });
+  console.log('Nerest is listening on port 3000');
+});
