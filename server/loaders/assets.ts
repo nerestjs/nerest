9ce27b8618ad43ee261828e9d3b57484c5b8c994
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2023-06-02 20:23:25 +0300

    feat: update app assets loading

--- server/loaders/assets.ts
+++ server/loaders/assets.ts
@@ -1,17 +1,22 @@
 import type { Manifest } from 'vite';
 
-// TODO: this is not as simple in the real world
+// TODO: this is not as simple in the real world, & remove hardcoded port
+// The path where the client files are deployed can be built-in during the initial build,
+// but the client scripts aren't using it, so maybe it should be a runtime env variable
+// for the server instead?
+// TODO: figure this out
 const publicPath = process.env.PUBLIC_PATH ?? 'http://0.0.0.0:3000/';
 
 // Extracts the list of assets for a given app from the manifest file
 export function loadAppAssets(manifest: Manifest, appName: string) {
-  const entries = Object.entries(manifest);
-
   // TODO: handling errors and potentially missing entries
-  const clientEntryJs = entries.find(([_, entry]) => entry.isEntry)?.[1].file;
-  const appCss =
-    entries.find(([name, _]) => name.includes(`/${appName}/index.tsx`))?.[1]
-      .css ?? [];
+  // All apps share the same JS entry that dynamically imports the chunks of the apps
+  // that are used on the page based on their name
+  const clientEntryJs =
+    manifest['node_modules/@nerest/nerest/client/index.ts'].file;
+
+  // Each app has its own CSS bundles, if it imports any CSS
+  const appCss = manifest[`apps/${appName}/index.tsx`].css ?? [];
 
   return [clientEntryJs, ...appCss].map((x) => publicPath + x);
 }
