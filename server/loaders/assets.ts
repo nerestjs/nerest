c355e8617f2c63b0c789bc78bd424fd22e089134
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2023-06-05 20:06:05 +0300

    feat: use `NEREST_STATIC_PATH` buildtime env variable

--- server/loaders/assets.ts
+++ server/loaders/assets.ts
@@ -1,14 +1,11 @@
 import type { Manifest } from 'vite';
 
-// TODO: this is not as simple in the real world, & remove hardcoded port
-// The path where the client files are deployed can be built-in during the initial build,
-// but the client scripts aren't using it, so maybe it should be a runtime env variable
-// for the server instead?
-// TODO: figure this out
-const publicPath = process.env.PUBLIC_PATH ?? 'http://0.0.0.0:3000/';
-
 // Extracts the list of assets for a given app from the manifest file
-export function loadAppAssets(manifest: Manifest, appName: string) {
+export function loadAppAssets(
+  appName: string,
+  manifest: Manifest,
+  staticPath: string
+) {
   // TODO: handling errors and potentially missing entries
   // All apps share the same JS entry that dynamically imports the chunks of the apps
   // that are used on the page based on their name
@@ -18,5 +15,5 @@ export function loadAppAssets(manifest: Manifest, appName: string) {
   // Each app has its own CSS bundles, if it imports any CSS
   const appCss = manifest[`apps/${appName}/index.tsx`].css ?? [];
 
-  return [clientEntryJs, ...appCss].map((x) => publicPath + x);
+  return [clientEntryJs, ...appCss].map((x) => staticPath + x);
 }
